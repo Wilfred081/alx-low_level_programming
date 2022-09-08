741ba87692c1a6430a9e34c2889f1500a661dcd1
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-08 06:37:11 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0);
+        return (0);
 
 }
