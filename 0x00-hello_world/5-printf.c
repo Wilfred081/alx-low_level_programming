2975dd1d6c804a65e7ba93d2645c149ff47bda3f
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-09 13:19:29 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - print a string
-* Description: use printf
+* main - entry point for the program
+*
 * Return: 0 if no errors, return non zero if errors
 */
 int main(void)
