694a5b87827b3e561357aea666fc9cb7850719e1
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-09 15:15:33 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {	
-printf("with proper grammar, but the outcome is a piece of art\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return i(0);
+return (0);
 }
