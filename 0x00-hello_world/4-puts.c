915b107b0d1e5818784378ed7addfeaca10e1cad
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-08 23:27:45 -0700

    4. Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 
+#include <stdio.h>
 /**
 *main - Entry point
-*Description: prints "\"programming is like building a multilingual puzzle"
+*
 *Return: 0
 */
-
 int main(void)
 {
           puts("\"programming is like building a multilingual puzzle"); 
