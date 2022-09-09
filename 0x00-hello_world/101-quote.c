cac37743cc64e717fabdf7db9999ce409197a295
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-09 16:40:09 -0700

    please work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - program entry point
  *
- * Return: 1 on success
- * */
+ * Description: prints and that piece..
+ * Return: 1 if successful 
+ */
 int main(void)
 {
-(2, "and that piece of art is useful\"- Doro Korpar, 2015-10-19\n",59);
+(2,"and that piece of art is useful\" - Doro Korpar, 2015-10-19\n",59);
 
 return (1);
 }
