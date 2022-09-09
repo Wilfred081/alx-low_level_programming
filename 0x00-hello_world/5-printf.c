bbecf4f14c13625562935e142b922de40393511c
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-09 12:59:42 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,11 @@
 /**
  * main - print a string
  * Description: use printf
- * Return: 0
+ * Return: 0 if no errors, return non zero if errors
  */
-
-  int main(void)
+int main(void)
 {	
-printf("%", "With proper grammar, but the outcome is a piece of art, \n");
+printf("With proper grammar, but the outcome is a piece of art, \n");
 
 return (0);
- }
+}
