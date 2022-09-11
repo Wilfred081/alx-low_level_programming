a46d008f24a72345f6abd5967f6c760603060a86
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-10 17:12:10 -0700

    task 2 you try

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 /**
 * main - Entry point
 *
@@ -10,10 +10,11 @@ int main(void)
 {
 char low;
 
-for (low = 'a' ; low <= 'z' ; low++)
+for (low = 'a'; low <= 'z'; low++)
 
 putchar(low);
 printf("\n");
 
 return (0);
+
 }
