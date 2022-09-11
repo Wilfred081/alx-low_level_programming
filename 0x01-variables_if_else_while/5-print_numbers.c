689563eef80772e7ec282ed42e7bba0158aac985
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-10 17:46:44 -0700

    do hardthings

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+int i;
+
+for (i = 0; i < 10; 1++)
+
+printf("%d", i);
+
+printf("\n");
+
+return (0);
+
+}
