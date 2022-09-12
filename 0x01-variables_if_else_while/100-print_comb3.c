38314a6b3e9ebfa72eb43ef50b62441a2d0f49ff
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-12 14:11:30 -0700

    are we good

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int main(void)
 int ones;
 int tens;
 
-for (tens = '0'; tens <= '9'; tens++) 
+for (tens = '0'; tens <= '9'; tens++)
  /*increment tens*/
 {
-for (ones = (tens + 1); ones <= '9'; ones++) 
+for (ones = (tens + 1); ones <= '9'; ones++)
  /*one's ten+1*/
 {
 putchar(tens);
