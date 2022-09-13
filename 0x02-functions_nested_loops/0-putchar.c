99e79baf26ef3a8ddd50d9eafe6e2e079649a850
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-13 09:39:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include "main."
 
 /**
-* main - print putchar
-* Return: Always 0
+* main - prints keyword _putchar
+*
+* Return: zero if exited properly
+*
 */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char str[] "_putchar";
+int ch;
+
+for (ch =0; ch < 8; ++ch)
+_putchar(str[ch]);
 _putchar('\n');
 
 return (0);
