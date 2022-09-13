bfbb8850274421861dad8dcf3b832a57df56ebdf
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-13 08:31:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDE
+#define HOLBERTON_H_INCLUDE
 
-#include <stdio.h>
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,9 +12,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int );
+int add(int a, int b );
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
 
-
-#endif /* MAIN_H*/
+#endif
