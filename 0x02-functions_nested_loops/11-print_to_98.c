7ed21daae2df782776ff37eda36a9103c5bf2612
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-14 03:41:04 -0700

    so cool

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_to_98 - prints all natural numbers from imput to 98,
+*              in order separated by a comma following by a space.
+* @n: The number to begin counting at.
+*/
+
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+}
