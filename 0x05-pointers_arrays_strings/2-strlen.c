a1b76024f9449fb3afa36ec6214a1bf4e3164b2c
Wilfred081 <wilfredaigbona96@gmail.com>
2022-09-19 01:27:04 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *_strlen - return the length of the string
- *@s: char type pointer
- *Return: length of string
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: length of string
  */
 
 int _strlen(char *s)
 {
-int i;
+   int i;
 
-for (i = 0; s[i] != '\0'; i++
-{
-}
+  for (i = 0; s[i] != '\0'; i++
+  {
+  }
 
-return (i);
+   return (i);
 }
